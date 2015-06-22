0644489ae3418f09bc9ddb45eb811e39b95af5f1
nomadsummer <xiatian0323@gmail.com>
2015-06-22 10:13:53 -0400

    Update UserInfo.java

--- src/insurance/UserInfo.java
+++ src/insurance/UserInfo.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -14,6 +15,7 @@ import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -36,11 +38,18 @@ public class UserInfo extends JFrame {
 	private JTextField cityField;
 	private JComboBox <String> stateBox;
 	private JTextField zipCodeField;
+	private JCheckBox female;
+	private JCheckBox male;
+	private JComboBox<String> violationBox;
 	private JButton save;
+	private JButton cancel;
 	
 	
 	private final String[] stateName = new String[]{"AK","AL","AZ","CO","CT","DE","GA","IL","IN","KS","LA","MD",
 			"MA","MS","MO","NV","NM","NY","NC","OH","OK","SC","TN","TX","WA","WY"};
+	private final String[] violationOption = new String[]{
+			"No violation during last 3 years","1-3 violations during last 3 years","3-5 violations during last 3 years",
+			"5-10 violations during last 3 years","more than 10 violations during last 3 years"};
 	
 	/**
 	 * Launch the application.
@@ -127,14 +136,98 @@ public class UserInfo extends JFrame {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				// TODO Auto-generated method stub
-				@SuppressWarnings("unchecked")
+				@SuppressWarnings({ "unchecked", "unused" })
 				JComboBox<String> jcb = (JComboBox<String>)e.getSource();
 				//String stName =(String)jcb.getSelectedItem();
 				
 			}
 			
 		});
-		//add(stateBox);
+		
+		JLabel gender = new JLabel("Gender");
+		gender.setBounds(50,270,68,16);
+		contentPane.add(gender);
+		
+		female = new JCheckBox("Female");
+		female.setMnemonic(KeyEvent.VK_G);
+		female.setSelected(false);
+		female.setBounds(150,230,100,100);
+		female.setVisible(true);
+		
+		male = new JCheckBox("Male");
+		male.setMnemonic(KeyEvent.VK_G);
+		male.setSelected(false);
+		male.setBounds(250,230,100,100);
+		male.setVisible(true);
+		
+		contentPane.add(female);
+		contentPane.add(male);
+		
+		/*female.addItemListener(new ItemListener(){
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				Object source = e.getItemSelectable();
+				int genderIndicator;
+				if(e.getStateChange() == ItemEvent.SELECTED){
+				BufferedWriter bw;
+				try {
+					bw = new BufferedWriter(new FileWriter("file.txt"));
+					bw.newLine();
+					bw.write("Gender: Female" + '\n');
+					bw.close();
+					genderIndicator = 2;
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				UserInfo.this.setVisible(false);
+					
+				}
+			}
+			
+		});
+		male.addItemListener(new ItemListener(){
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				Object source = e.getItemSelectable();
+				if(e.getStateChange() == ItemEvent.SELECTED){
+				BufferedWriter bw;
+				try {
+					bw = new BufferedWriter(new FileWriter("file.txt"));
+					bw.newLine();
+					bw.write("Gender: Female" + '\n');
+					bw.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				UserInfo.this.setVisible(false);
+					
+				}
+			}
+			
+		});*/
+		
+		JLabel violation = new JLabel("Violation History");
+		violation.setBounds(50, 330, 80, 30);
+		contentPane.add(violation);
+		
+		violationBox = new JComboBox<String>(violationOption);
+		violationBox.setBounds(140,200,300,300);
+		contentPane.add(violationBox);
+		violationBox.addItemListener(new ItemListener(){
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
 		save = new JButton("Save and Continue");
 		save.setBounds(260, 420, 200, 29);
@@ -153,9 +246,6 @@ public class UserInfo extends JFrame {
 							+ "State: "+ stateBox.getSelectedItem() + '\n'
 							+"zip code: " + zipCodeField.getText() + '\n');
 					bw.close();
-					UserInfo.this.setVisible(false);
-					AddVehicle av = new AddVehicle();
-					av.setVisible(true);
 				}catch (FileNotFoundException e1)
 				{
 					e1.printStackTrace();
@@ -166,11 +256,14 @@ public class UserInfo extends JFrame {
 					e1.printStackTrace();
 				}
 				ui.setVisible(true);
+				AddVehicle av = new AddVehicle();
+				av.setVisible(true);
+				UserInfo.this.setVisible(false);
 			}
 		});
 		contentPane.add(save);
 		
-		JButton cancel = new JButton("cancel");
+		cancel = new JButton("cancel");
 		cancel.setBounds(70, 420, 117, 29);
 		cancel.addActionListener(new ActionListener(){
 
@@ -191,7 +284,7 @@ public class UserInfo extends JFrame {
 		
 		contentPane.add(imgLabe2);
 	}
-	public void clearcombo(JComboBox model){
+	public void clearcombo(JComboBox<String> model){
 		int itemCount = model.getItemCount();
 	    for(int i=0;i<itemCount;i++){
 	        model.removeItemAt(0);
@@ -200,4 +293,3 @@ public class UserInfo extends JFrame {
 	
 		
 	}
-
